662487b3c55d1061e760957e972c0030a67f0c80
AgroVision789 <preranawagh2003@gmail.com>
2025-06-14 01:37:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,9 @@ def predict_price():
 
     return jsonify({"forecast": forecast})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
